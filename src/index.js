46e2354f17310ba0cbe018ea34fce9382f38263c
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-06-27 16:26:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+import displayMovies from './modules/displayMovies.js';
+
+window.addEventListener('load', () => {
+  displayMovies();
+  // const mainContainer = document.getElementById('mainContainer');
+
+  //   const nextSlide = () => {
+  //     mainContainer.scrollLeft -= 700;
+  //   };
+
+  //   const prevSlide = () => {
+  //     mainContainer.scrollLeft += 700;
+  //   };
+
+//   prevBtn.addEventListener('click', nextSlide);
+//   nextBtn.addEventListener('click', prevSlide);
+});
